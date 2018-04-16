dc6873df46189a8ed40d435da6e06f2ad4a75cad
studiefredfredrik <studiefredfredrik@gmail.com>
2018-04-16 22:00:54 +0200

    experimenting with singelton

--- vue-todo-api/vue-todo-api/Startup.cs
+++ vue-todo-api/vue-todo-api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
 using Raven.Client.Documents;
 using System.IO;
 
@@ -21,10 +22,14 @@ namespace vue_todo_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            var store = InitRavenDb();
+            services.AddSingleton(store);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -34,12 +39,6 @@ namespace vue_todo_api
             option.AddRedirect("^$", "index.html"); // Matches on empty string
             app.UseRewriter(option);
 
-
-            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
-            //defaultFilesOptions.DefaultFileNames.Clear();
-            //defaultFilesOptions.DefaultFileNames.Add("index.html");
-            //app.UseDefaultFiles(defaultFilesOptions);
-
             var currentDir = Directory.GetCurrentDirectory();
             var distFolder = Path.Combine(currentDir, "../../vue-todo-frontend/dist");
             var staticFileOptions = new StaticFileOptions();
@@ -47,14 +46,18 @@ namespace vue_todo_api
             app.UseStaticFiles(staticFileOptions);
 
             app.UseMvc();
-
-            //app.UseStaticFiles(new StaticFileOptions
-            //{
-            //    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "../../vue-todo-frontend/dist")), RequestPath = ""
-            //});
-
-
-
         }
+
+        private static IDocumentStore InitRavenDb()
+        {
+            var store = new DocumentStore
+            {
+                Urls = new[] { "http://localhost:8080" },
+                Database = "vue-todo-notes"
+            };
+            store.Initialize();
+            return store;
+        }
+
     }
 }
