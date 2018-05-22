767c2bdf73442158128a52169910de42c34c99ee
studiefredfredrik <studiefredfredrik@gmail.com>
2018-05-22 22:50:19 +0200

    yay, loading props if exist, found a way

--- vue-todo-frontend/src/components/Modal.vue
+++ vue-todo-frontend/src/components/Modal.vue
@@ -9,26 +9,26 @@
         <!-- Blog entry -->
         <div class="w3-card-4 w3-margin w3-white ">
           <img src="/w3images/bridge.jpg" alt="Norway" style="width:100%">
-          <croppa v-model="post.image.myCroppa"
+          <croppa v-model="image.myCroppa"
                   :width="700"
                   :height="220"
           ></croppa>
           <div class="w3-container">
-            <input type="text" v-if="post.heading.editing" v-on:blur="post.heading.editing = false;" v-model="post.heading.text"/>
-            <h3><b v-if="!post.heading.editing" v-on:click="post.heading.editing = true;">{{post.heading.text}}</b></h3>
-            <input type="text" v-if="post.undertitle.editing" v-on:blur="post.undertitle.editing = false;" v-model="post.undertitle.text"/>
-            <h5 v-if="!post.undertitle.editing" v-on:click="post.undertitle.editing = true;">{{post.undertitle.text}}</h5>
+            <input type="text" v-if="heading.editing" v-on:blur="heading.editing = false;" v-model="heading.text"/>
+            <h3><b v-if="!heading.editing" v-on:click="heading.editing = true;">{{heading.text}}</b></h3>
+            <input type="text" v-if="undertitle.editing" v-on:blur="undertitle.editing = false;" v-model="undertitle.text"/>
+            <h5 v-if="!undertitle.editing" v-on:click="undertitle.editing = true;">{{undertitle.text}}</h5>
           </div>
 
           <div class="w3-container">
-            <input type="text" class="widt100" v-if="post.description.editing" v-on:blur="post.description.editing = false;" v-model="post.description.text"/>
-            <p id="description" v-if="!post.description.editing" v-on:click="post.description.editing = true;">
-              {{post.description.text}}
+            <input type="text" class="widt100" v-if="description.editing" v-on:blur="description.editing = false;" v-model="description.text"/>
+            <p id="description" v-if="!description.editing" v-on:click="description.editing = true;">
+              {{description.text}}
             </p>
 
-            <input class="widt100" type="text" v-if="post.more.editing" v-on:blur="post.more.editing = false;" v-model="post.more.text"/>
-            <p v-if="!post.more.editing" v-on:click="post.more.editing = true;">
-              {{post.more.text}}
+            <input class="widt100" type="text" v-if="more.editing" v-on:blur="more.editing = false;" v-model="more.text"/>
+            <p v-if="!more.editing" v-on:click="more.editing = true;">
+              {{more.text}}
             </p>
             <br>
           </div>
@@ -53,49 +53,45 @@
   export default {
     name: 'modal',
     data: function() {
-      return {}
-    },
-    props: {
-      post:{
-        default: () => ({
-          image: {
-            myCroppa: {},
-            image: null,
-            editing: false,
-          },
-          heading: {
-            text: `BLOG ENTRY`,
-            editing: false,
-          },
-          undertitle: {
-            text: `Praesent tincidunt sed`,
-            editing: false,
-          },
-          description: {
-            text: `Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl.
+      return {
+        image: {
+          myCroppa: {},
+          image: null,
+          editing: false,
+        },
+        heading: {
+          text: `BLOG ENTRY`,
+          editing: false,
+        },
+        undertitle: {
+          text: `Praesent tincidunt sed`,
+          editing: false,
+        },
+        description: {
+          text: `Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl.
                   Sed mattis nunc id lorem euismod placerat.
                   Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper.
                   Praesent tincidunt sed
                   tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.`,
-            editing: false,
-          },
-          more: {
-            text: `Even more info here, with more details etc. And more more more`,
-            editing: false,
-          },
-        })
+          editing: false,
+        },
+        more: {
+          text: `Even more info here, with more details etc. And more more more`,
+          editing: false,
+        },
       }
     },
+    props: ['post'],
     methods: {
       savePost: function(){
-        console.log(this.post.image.myCroppa.generateDataUrl());
+        console.log(this.image.myCroppa.generateDataUrl());
 
         let post = {
-          image: this.post.image.myCroppa.generateDataUrl(),
-          heading: this.post.heading.text,
-          undertitle: this.post.undertitle.text,
-          description: this.post.description.text,
-          more: this.post.more.text,
+          image: this.image.myCroppa.generateDataUrl(),
+          heading: this.heading.text,
+          undertitle: this.undertitle.text,
+          description: this.description.text,
+          more: this.more.text,
           type: 'equipment'
         }
         console.log('post', post);
@@ -113,6 +109,12 @@
         this.$emit('close');
       },
     },
+    mounted(){
+      console.log(this.post)
+      if(this.post)
+        this.heading.text = this.post.heading
+
+    }
   }
 </script>
 
