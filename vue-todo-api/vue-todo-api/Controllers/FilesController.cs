0c35e15ccf4a7464db4f2219a0836d21def47234
samtidog <veggisfredrik@gmail.com>
2018-10-15 22:16:49 +0200

    File upload working

--- vue-todo-api/vue-todo-api/Controllers/FilesController.cs
+++ vue-todo-api/vue-todo-api/Controllers/FilesController.cs
@@ -6,27 +6,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using Raven.Client.Documents;
 using VueTodoApi.Configuration;
-using VueTodoApi.Models;
 
 namespace VueTodoApi.Controllers
 {
+    [Route("api/[controller]")]
     public class FilesController : Controller
     {
         private readonly FilesSettings _filesSettings;
-        readonly IDocumentStore _store;
         
         public FilesController(FilesSettings filesSettings, IDocumentStore store)
         {
             _filesSettings = filesSettings;
-            _store = store;
         }
 
         [DisableRequestSizeLimit]
         [Authorize]
-        [HttpGet("{noteId}/{fileName}")]
-        public IActionResult GetFile(string noteId, string fileName)
+        [HttpGet("{folder}/{fileName}")]
+        public IActionResult GetFile(string folder, string fileName)
         {
-            var path = $"{_filesSettings.Path}/{noteId}/{fileName}";
+            var path = $"{_filesSettings.Path}/{folder}/{fileName}";
 
             if (path.Contains("..")) return Forbid();
             if (!System.IO.File.Exists(path)) return NotFound();
@@ -39,14 +37,19 @@ namespace VueTodoApi.Controllers
         [DisableRequestSizeLimit]
         [Authorize]
         [HttpPost]
-        public async Task<IActionResult> Post(string noteId, string fileName)
+        public async Task<IActionResult> Post(string folder, string fileName, bool overwrite = false)
         {
-            if (string.IsNullOrWhiteSpace(noteId) || string.IsNullOrWhiteSpace(fileName)) return BadRequest();
+            if (string.IsNullOrWhiteSpace(folder) || string.IsNullOrWhiteSpace(fileName)) return BadRequest();
             
-            var path = $"{_filesSettings.Path}/{noteId}/{fileName}";
+            var path = $"{_filesSettings.Path}/{folder}/{fileName}";
 
             if (path.Contains("..")) return Forbid();
-            if (System.IO.File.Exists(path)) return Forbid();
+
+            if (System.IO.File.Exists(path))
+            {
+                if(overwrite) System.IO.File.Delete(path);
+                else return Forbid();
+            }
             
             var file = Request.Form.Files[0];
             var size = file.Length;
@@ -54,21 +57,12 @@ namespace VueTodoApi.Controllers
             if (size > 0)
             {
                 using (var fileStream = System.IO.File.Create(path))
-                using (var session = _store.OpenAsyncSession())
                 {
-                    
-                    var note = await session.LoadAsync<NotesDocument>(noteId);
-                    if(note.Files == null) note.Files = new List<string>();
-                    if(!note.Files.Contains(fileName)) note.Files.Add(fileName);
-                    await session.StoreAsync(note);
-                    await session.SaveChangesAsync();
-
                     fileStream.Seek(0, SeekOrigin.Begin);
                     await file.CopyToAsync(fileStream);
                     fileStream.Close();
-                    
-                    return Ok(new {fileName});
                 }
+                return Ok(new {fileName});
             }
             return BadRequest();
         }
